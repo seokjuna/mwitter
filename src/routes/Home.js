fd4796cba8a0eb22fa7d704535b0cf85ae75a7cf
naseokju <lukas95@naver.com>
2023-03-02 16:10:58 +0900

    Mweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,24 +1,36 @@
 import React, { useState } from "react";
+import { dbService } from "../fbase";
+import { addDoc, collection } from "firebase/firestore";
 
 const Home = () => {
-    const [nweet, setNweet] = useState("");
+    const [mweet, setMweet] = useState("");
     
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
+        try {
+            const docRef = await addDoc(collection(dbService, "mweets"), {
+                mweet,
+                createdAt: Date.now(),
+            });
+            console.log("Document written with ID: ", docRef);
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+        setMweet("");
     };
 
     const onChange = (e) => {
         const {
             target: { value },
         } = e;
-        setNweet(value);
+        setMweet(value);
     }
 
     return (
         <div>
             <form onSubmit={onSubmit}>
                 <input
-                    value={nweet}
+                    value={mweet}
                     type="text"
                     placeholder="What's on your mind?"
                     onChange={onChange}
@@ -26,7 +38,7 @@ const Home = () => {
                 />
                 <input
                     type="submit"
-                    value="Nweet"
+                    value="Mweet"
                 />
             </form>
         </div>
