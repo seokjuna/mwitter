2b98337e0b560acb6d698d7fff63118837f5e108
naseokju <lukas95@naver.com>
2023-03-02 15:15:34 +0900

    Creating Account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { authService } from "../fbase";
 
 const Auth = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
     
     const onChange = (e) => {
         const {
@@ -15,13 +18,27 @@ const Auth = () => {
         }
     };
 
-    const onSubmit = (e) => {
+    const onSubmit = async (e) => {
         e.preventDefault();
+        try {
+            let data;
+            if (newAccount) {
+                // create account
+                data = await createUserWithEmailAndPassword(authService, email, password);
+            } else {
+                // login
+                data = await signInWithEmailAndPassword(authService, email, password);
+            }
+            console.log(data);
+        } catch (error) {
+            console.log(error);
+        }
+
     };
 
     return (
-        <div onSubmit={onSubmit}>
-            <form>
+        <div>
+            <form onSubmit={onSubmit}>
                 <input
                     name="email"
                     type="text"
@@ -40,7 +57,7 @@ const Auth = () => {
                 />
                 <input
                     type="submit"
-                    value="Log In"
+                    value={newAccount ? "Create Account" : "Sign In"}
                 />
             </form>
             <div>
