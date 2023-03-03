1b5007ede52683d8cb279f9bc67520fa0e65c368
naseokju <lukas95@naver.com>
2023-03-03 15:05:21 +0900

    Update Profile

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,14 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { authService, dbService } from "../fbase";
 import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { getAuth, updateProfile } from "firebase/auth";
 
 const Profile = ({ userObj }) => {
     const navigate = useNavigate();
+    const [newDisplayName, setNewDisplayName] = useState(userObj.newDisplayName);
 
     const onLogOutClick = () => {
         authService.signOut();
         navigate("/");
+    };
+
+    const onChange = (e) => {
+        const {
+            target: { value },
+        } = e;
+        setNewDisplayName(value);
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        const auth = getAuth();
+        if (userObj.displayName !== newDisplayName) {
+            await updateProfile(auth.currentUser, { displayName: newDisplayName });
+        };
     };
 
     const getMyMweets = async () => {
@@ -28,9 +45,23 @@ const Profile = ({ userObj }) => {
     }, []);
 
     return (
-        <button onClick={onLogOutClick}>
-            Log Out
-        </button>
+        <>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    placeholder="Display name"
+                    value={newDisplayName || ""}
+                    onChange={onChange}
+                />
+                <input
+                    type="submit"
+                    value="Update profile"
+                />
+            </form>
+            <button onClick={onLogOutClick}>
+                Log Out
+            </button>
+        </>
     )
 }
 
