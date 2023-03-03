152f34445305fe7346ec78b9943c23236a459f9c
naseokju <lukas95@naver.com>
2023-03-03 14:55:12 +0900

    Get My Own Mweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { authService } from "../fbase";
+import { authService, dbService } from "../fbase";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
 
-const Profile = () => {
+const Profile = ({ userObj }) => {
     const navigate = useNavigate();
+
     const onLogOutClick = () => {
         authService.signOut();
         navigate("/");
-    }
+    };
+
+    const getMyMweets = async () => {
+        const q = query(
+            collection(dbService, "mweets"),
+            where("creatorId", "==", `${userObj.uid}`),
+            orderBy("createdAt", "desc")
+        );
+        const querySnapshot = await getDocs(q);
+        querySnapshot.forEach((document) => {
+            console.log(document.id, "=>", document.data);
+        });
+    };
+
+    useEffect(() => {
+        getMyMweets();
+    }, []);
 
     return (
         <button onClick={onLogOutClick}>
